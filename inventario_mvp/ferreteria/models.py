0da43d724789d077643c21ed1e3a7f5fea4a821c
Elias <eliasantican14@gmail.com>
2025-07-07 17:06:37 -0400

    no 0

--- inventario_mvp/ferreteria/models.py
+++ inventario_mvp/ferreteria/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0, message="El precio no puede ser menor a 0.")])
     stock = models.PositiveIntegerField()
 
     def __str__(self):
